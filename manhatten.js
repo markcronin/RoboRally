4ccb4238bc7d0d1fe9099a8658bfe48e021cdb61
Mark Cronin <mark_cronin@icloud.com>
2018-04-29 15:57:24 +1000

    changed manhattenDistance function

--- manhatten.js
+++ manhatten.js
@@ -2,11 +2,7 @@ const botPos = [3, 5];
 const targetPos = [8, -1];
 
 function manhattenDistance(botCoord, targetCoord) {
-  let xAxis = botCoord[0] < targetCoord[0] ?
-              (targetCoord[0] - botCoord[0]) : (botCoord[0] - targetCoord[0]);
-  let yAxis = botCoord[1] < targetCoord[1] ?
-              (targetCoord[1] - botCoord[1]) : (botCoord[1] - targetCoord[1]);
-  return xAxis + yAxis
+  return (Math.abs(botPos[0] - targetPos[0])) + (Math.abs(botPos[1] - targetPos[1]))
 }
 
 console.log(manhattenDistance(botPos,targetPos))
