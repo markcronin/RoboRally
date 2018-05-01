29bc8dea1987cbae003b38a26991224b4f1ccdee
Mark Cronin <mark_cronin@icloud.com>
2018-05-01 21:44:35 +1000

    cleaned up moveBot.js

--- moveBot.js
+++ moveBot.js
@@ -10,66 +10,71 @@ const squashBot = {
   alive: true
 };
 
+const directions = ['north', 'east', 'south', 'west'];
+const moveKey =  {b: -1, 1: 1, 2: 2, 3: 3, l: -1, r: 1, u: 2};
+const moveValue = {b: -1, 1: 1, 2: 1, 3: 1, l: -1, r: 1, u: 1};
+
+// changes the orientation of the robot, given the move (l, r, u)
+function changeOrientation(robot, move) {
+  let orientationIndex = _.indexOf(directions, robot.orientation) + moveValue[move];
+  robot.orientation = directions[(orientationIndex + 4) % 4];
+}
+
+// changes the position of the robot, given the move (1, 2, 3, b)
+function changePosition(robot, move) {
+  if (robot.orientation === 'north') {
+    robot.position[1] -= moveValue[move];
+  } else if (robot.orientation === 'south') {
+    robot.position[1] += moveValue[move];
+  } else if (robot.orientation === 'east') {
+    robot.position[0] += moveValue[move];
+  } else {
+    robot.position[0] -= moveValue[move];
+  }
+}
+
+// returns whether or not the robot is blocked by a wall given
+// its current position and orientation.
+function blockedByWall(robot, board){
+  let currentOrientation = robot.orientation;
+  let currentSquare = board[ robot.position[1] ][ robot.position[0] ];
+  return currentSquare[currentOrientation];
+}
+
+function checkPit(robot, board) {
+  let currentSquare = board[ robot.position[1] ][ robot.position[0] ]
+  if (currentSquare.type === "pit") {
+    robot.alive = false;
+  }
+  //returns true if the robot dies
+  return !robot.alive;
+}
+
 
 function moveRobotOneStep(robot, move, board) {
   let newBot = _.cloneDeep(robot);
-  const directions = ['north', 'east', 'south', 'west'];
-  const moveKey =  {b: -1, 1: 1, 2: 2, 3: 3, l: -1, r: 1, u: 2};
-  const moveKey2 = {b: -1, 1: 1, 2: 1, 3: 1, l: -1, r: 1, u: 1};
-  // checks to see if the bot is still alive
   if (newBot.alive === false) {
     console.log ("you are dead");
     return newBot;
-  } else {
-    // for loop steps the bot though each square on the board
-    // instead of just teleporting to the final destination
-    for (let x = Math.abs(moveKey[move]); x > 0; x--) {
-      // "l" and "r" handle left and right rotations
-      // "u" handles 180 degree U-turns
-      if ( move === "l" || move === "r" || move === "u") {
-        let y = _.indexOf(directions, newBot.orientation) + moveKey2[move];
-        newBot.orientation = directions[(y + 4) % 4];
-      } else {
-        // checks to see if there is a wall blocking the way
-        // breaks the loop before moving the bot if there is
-        let currentOrientation = newBot.orientation
-        let currentSquare = testBoard[ newBot.position[1] ][ newBot.position[0] ]
-        if (currentSquare[currentOrientation] === true){
-          console.log ("you bumped into a wall");
-          break;
-        } else {
-          // steps the bot forward (1,2,3) or backwards (b)
-          // based on its orientation
-          if (newBot.orientation === 'north') {
-            newBot.position[1] -= moveKey2[move];
-          } else if (newBot.orientation === 'south') {
-            newBot.position[1] += moveKey2[move];
-          } else if (newBot.orientation === 'east') {
-            newBot.position[0] += moveKey2[move];
-          } else {
-            newBot.position[0] -= moveKey2[move];
-          }
-          // checks to see if the new destination square is a pit
-          // kills the bot and breaks the current loop if true
-          let currentSquare = testBoard[ newBot.position[1] ][ newBot.position[0] ]
-          if (currentSquare.type === "pit") {
-            newBot.alive = false;
-            console.log ("you died");
-            break;
-          } else {
-            // logs each sqaure the bot encounrters as it travels
-            // for testing purposes
-            console.log (newBot.position);
-          }
-        }
-      }
+  }
+  for (let x = Math.abs(moveKey[move]); x > 0; x--) {
+    if ( move === "l" || move === "r" || move === "u") {
+      changeOrientation (newBot, move);
+      continue;
+    }
+    if (blockedByWall (newBot, board)) {
+      break;
+    }
+    changePosition(newBot, move);
+    if (checkPit(newBot, board)) {
+      break;
     }
   }
   return newBot;
 }
 
-//console.log (squashBot)
+console.log (squashBot)
 //console.log (moveRobotOneStep(squashBot, "l", testBoard))
-//console.log (moveRobotOneStep(squashBot, 3, testBoard))
+console.log (moveRobotOneStep(squashBot, 3, testBoard))
 
 module.exports = moveRobotOneStep;
